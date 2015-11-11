acc7c81532f3a0d955bf8c37f6de8b8d7dc7c6fc
Mario Cecchi <macecchi@gmail.com>
2015-11-11 03:42:11 -0200

    Melhorias na tela de perfil

--- Caronae/ProfileViewController.h
+++ Caronae/ProfileViewController.h
@@ -9,4 +9,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *courseLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *profileImage;
 
+// Numbers
+@property (weak, nonatomic) IBOutlet UILabel *joinedDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numDrivesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numRidesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numLikesLabel;
+
+// Car details
+@property (weak, nonatomic) IBOutlet UILabel *carPlateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *carModelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *carColorLabel;
+
 @end
--- Caronae/ProfileViewController.m
+++ Caronae/ProfileViewController.m
@@ -1,21 +1,35 @@
 #import "ProfileViewController.h"
 
 @interface ProfileViewController ()
-
+@property (nonatomic) NSDateFormatter *joinedDateFormatter;
 @end
 
 @implementation ProfileViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSDictionary *user = [[NSUserDefaults standardUserDefaults] objectForKey:@"user"];
+    
+    NSDateFormatter *joinedDateParser = [[NSDateFormatter alloc] init];
+    joinedDateParser.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    NSDate *joinedDate = [joinedDateParser dateFromString:user[@"created_at"]];
+    self.joinedDateFormatter = [[NSDateFormatter alloc] init];
+    self.joinedDateFormatter.dateFormat = @"MM/yyyy";
+    
+    self.nameLabel.text = user[@"name"];
+    self.courseLabel.text = user[@"course"];
+   
+    self.joinedDateLabel.text = [self.joinedDateFormatter stringFromDate:joinedDate];
+    
+    self.carPlateLabel.text = user[@"car_plate"];
+    self.carModelLabel.text = user[@"car_model"];
+    self.carColorLabel.text = user[@"car_color"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 - (IBAction)didTapLogoutButton:(id)sender {
+    // TODO: Add confirmation dialog
+    [self performSegueWithIdentifier:@"AuthScreen" sender:self];
+    [[NSUserDefaults standardUserDefaults] setObject:nil forKey:@"user"];
 }
 
 @end
