46f9b0fdbb6ed7071dd2b02d6389dba1fadefc36
Rafael Damasceno <rdamasceno@poli.ufrj.br>
2016-09-30 12:48:33 -0300

    Atualiza arquivos necessários para Swift 3.0

--- Caronae/Categories/UIImage+imageWithTint.swift
+++ Caronae/Categories/UIImage+imageWithTint.swift
@@ -1,29 +1,29 @@
 import UIKit
 
 extension UIImage {
-    func imageWithTintColor(color: UIColor) -> UIImage {
-        let rect = CGRect(origin: CGPointZero, size: self.size)
+    func imageWithTintColor(_ color: UIColor) -> UIImage {
+        let rect = CGRect(origin: CGPoint.zero, size: self.size)
         UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
         let context = UIGraphicsGetCurrentContext()
-        self.drawInRect(rect)
+        self.draw(in: rect)
         color.setFill()
-        CGContextSetBlendMode(context, CGBlendMode.SourceAtop)
-        CGContextFillRect(context, rect)
+        context?.setBlendMode(CGBlendMode.sourceAtop)
+        context?.fill(rect)
         let result = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return result
+        return result!
     }
     
-    func imageWithRed(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIImage {
-        let rect = CGRect(origin: CGPointZero, size: self.size)
+    func imageWithRed(_ red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIImage {
+        let rect = CGRect(origin: CGPoint.zero, size: self.size)
         UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
         let context = UIGraphicsGetCurrentContext()
-        self.drawInRect(rect)
-        CGContextSetRGBFillColor(context, red, green, blue, alpha)
-        CGContextSetBlendMode(context, CGBlendMode.SourceAtop)
-        CGContextFillRect(context, rect)
+        self.draw(in: rect)
+        context?.setFillColor(red: red, green: green, blue: blue, alpha: alpha)
+        context?.setBlendMode(CGBlendMode.sourceAtop)
+        context?.fill(rect)
         let result = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return result
+        return result!
     }
 }
