104a8a1fce40b390a04bf84f24b47b79edf7592b
Mario Cecchi <macecchi@gmail.com>
2016-12-29 11:13:53 -0200

    Remove teste desnecessário

--- Caronae/Networking/CaronaeAPIHTTPSessionManager.swift
+++ Caronae/Networking/CaronaeAPIHTTPSessionManager.swift
@@ -14,18 +14,11 @@ class CaronaeAPIHTTPSessionManager: AFHTTPSessionManager {
 
 class CaronaeRequestSerializer: AFJSONRequestSerializer {
     override func request(withMethod method: String, urlString URLString: String, parameters: Any?, error: NSErrorPointer) -> NSMutableURLRequest {
-        // Add user token to the HTTP headers if the user is logged in
-        if let userToken = UserController.sharedInstance().userToken {
-            self.setValue(userToken, forHTTPHeaderField: "token")
-        } else {
-            self.setValue(nil, forHTTPHeaderField: "token")
-        }
+        // Add user token to the HTTP headers
+        self.setValue(UserController.sharedInstance().userToken, forHTTPHeaderField: "token")
+
         // Add user FB token to the HTTP headers
-        if let userFBToken = UserController.sharedInstance().userFBToken {
-            self.setValue(userFBToken, forHTTPHeaderField: "Facebook-Token")
-        } else {
-            self.setValue(nil, forHTTPHeaderField: "Facebook-Token")
-        }
+        self.setValue(UserController.sharedInstance().userFBToken, forHTTPHeaderField: "Facebook-Token")
         
         return super.request(withMethod: method, urlString: URLString, parameters: parameters, error: error)
     }
