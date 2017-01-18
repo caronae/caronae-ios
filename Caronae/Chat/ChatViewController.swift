24c808ef3a59cd9273abcc5f504db2dc0b61234b
Rafael Damasceno <rdamasceno@poli.ufrj.br>
2017-01-18 16:40:40 -0200

    Combina ChatViewController.swift com ChatViewController+Messages.swift

--- Caronae/Chat/ChatViewController.swift
+++ Caronae/Chat/ChatViewController.swift
@@ -248,5 +248,42 @@ class ChatViewController: JSQMessagesViewController {
         }
         return true
     }
-
+    
+    
+    // MARK: ChatService and Notification methods
+    
+    func loadChatMessages() {
+        // Load local messages
+        ChatService.instance.messagesForRide(withID: ride.id) { messages, error in
+            guard error == nil else {
+                NSLog("Whoops, couldn't load: %@", error!.localizedDescription)
+                return
+            }
+            
+            self.messages = messages
+            self.subscribeToChanges()
+        }
+        
+        // Check for updates
+        ChatService.instance.updateMessagesForRide(withID: ride.id) { error in
+            guard error == nil else {
+                NSLog("Error updating messages for ride %ld. (%@)", self.ride.id, error!.localizedDescription)
+                return
+            }
+            
+            NSLog("Updated messages for ride %ld", self.ride.id)
+        }
+    }
+    
+    func subscribeToChanges() {
+        messagesNotificationToken = messages.addNotificationBlock { [weak self] (changes: RealmCollectionChange) in
+            self?.finishReceivingMessage()
+        }
+    }
+    
+    func clearNotifications() {
+        NotificationService.instance.clearNotifications(forRideID: ride.id, of: .chat)
+    }
+    
 }
+
