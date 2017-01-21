46a5138eb87fe49835960bae1239d710bb676768
Mario Cecchi <macecchi@gmail.com>
2017-01-21 12:38:25 -0200

    Remove comentários de migração do Realm desnecessária

--- Caronae/AppDelegate+Realm.swift
+++ Caronae/AppDelegate+Realm.swift
@@ -10,12 +10,7 @@ extension AppDelegate {
             // Set the block which will be called automatically when opening a Realm with
             // a schema version lower than the one set above
             migrationBlock: { migration, oldSchemaVersion in
-                // We haven’t migrated anything yet, so oldSchemaVersion == 0
-                if (oldSchemaVersion < 1) {
-                    // Nothing to do!
-                    // Realm will automatically detect new properties and removed properties
-                    // And will update the schema on disk automatically
-                }
+                // We haven’t migrated anything yet
         })
         
         // Tell Realm to use this new configuration object for the default Realm
