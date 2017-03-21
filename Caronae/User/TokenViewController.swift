acc7685d62eba83b86b49108305cecec41b1e814
Mario Cecchi <macecchi@gmail.com>
2017-03-20 23:48:32 -0300

    Simplificando design do login com usuário e senha

--- Caronae/User/TokenViewController.swift
+++ Caronae/User/TokenViewController.swift
@@ -4,12 +4,12 @@ import UIKit
 
 class TokenViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
-    @IBOutlet weak var authButton: UIButton!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var tokenTextField: UITextField!
     @IBOutlet weak var authFieldsView: UIView!
     @IBOutlet weak var buttonsView: UIView!
     @IBOutlet weak var bottomBackgroundView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     static func tokenViewController() -> TokenViewController {
         let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "InitialTokenScreen") as! TokenViewController
@@ -17,12 +17,11 @@ class TokenViewController: UIViewController {
         return viewController
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        authButton.isEnabled = false
-        tokenTextField.delegate = self
-        idTextField.delegate = self
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if authFieldsView.alpha != 0 {
+            idTextField.becomeFirstResponder()
+        }
     }
     
     func authenticateWithUserAndToken() {
@@ -32,7 +31,6 @@ class TokenViewController: UIViewController {
     }
 
     func authenticate(withUser user: String, token: String) {
-        authButton.isEnabled = false
         SVProgressHUD.show()
         
         UserService.instance.signIn(withUser: user, token: token, success: { _ in
@@ -52,7 +50,6 @@ class TokenViewController: UIViewController {
             
             SVProgressHUD.dismiss()
             CaronaeAlertController.presentOkAlert(withTitle: "Não foi possível autenticar", message: errorMessage)
-            self.authButton.isEnabled = true
         })
     }
     
@@ -63,19 +60,16 @@ class TokenViewController: UIViewController {
         SignInViewController.presentFromViewController(self, delegate: self)
     }
     
-    @IBAction func signInWithCredentials() {        
+    @IBAction func signInWithCredentials() {
+        idTextField.becomeFirstResponder()
+        
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
-            self.bottomBackgroundView.frame.origin.y += 30
-            self.authFieldsView.frame.origin.y += 30
             self.buttonsView.alpha = 0
             self.authFieldsView.alpha = 1
+            self.welcomeLabel.alpha = 0
+            self.bottomBackgroundView.alpha = 0
         })
     }
-    
-    @IBAction func didTapAuthenticateButton() {
-        authenticateWithUserAndToken()
-    }
-    
 }
 
 extension TokenViewController: CaronaeSignInDelegate {
@@ -90,28 +84,9 @@ extension TokenViewController: CaronaeSignInDelegate {
 
 extension TokenViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        switch textField {
-        case tokenTextField:
-            authenticateWithUserAndToken()
-            return false
-        case idTextField:
-            if idTextField.hasText {
-                tokenTextField.becomeFirstResponder()
-            }
-        default:
-            break
-        }
-        
-        return true
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let text = (textField.text as NSString!).replacingCharacters(in: range, with: string)
-        switch textField {
-        case idTextField:
-            authButton.isEnabled = !text.isEmpty && tokenTextField.hasText
-        default:
-            authButton.isEnabled = !text.isEmpty && idTextField.hasText
+        if textField == idTextField {
+            tokenTextField.becomeFirstResponder()
+        } else if textField == tokenTextField && idTextField.hasText && tokenTextField.hasText {            authenticateWithUserAndToken()
         }
         
         return true
