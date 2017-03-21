284d65f278542c2944ad7c54d86e166e69a5273e
Mario Cecchi <macecchi@gmail.com>
2017-03-21 01:07:08 -0300

    Prepara mudança da cor do TextField quando está ativo
    Ainda não está ativa pois há outros elementos que replicam o mesmo estilo e precisam ser adaptados

--- Caronae/Extras/CaronaePhoneTextField.m
+++ Caronae/Extras/CaronaePhoneTextField.m
@@ -1,5 +1,11 @@
 #import "CaronaePhoneTextField.h"
 
+@interface CaronaePhoneTextField ()
+@property (nonatomic) BOOL isActive;
+@property (nonatomic, strong) CALayer *bottomLine;
+@end
+
+
 @implementation CaronaePhoneTextField
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
@@ -13,14 +19,39 @@
 - (void)layoutSubviews {
     self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 32.0);
     
-    CALayer *bottomLine = [CALayer layer];
-    bottomLine.frame = CGRectMake(0.0f, self.frame.size.height - 1.0f, self.frame.size.width, 1.0f);
-    bottomLine.backgroundColor = [UIColor colorWithRed:0.576 green:0.580 blue:0.576 alpha:1.000].CGColor;
     self.borderStyle = UITextBorderStyleNone;
-    [self.layer addSublayer:bottomLine];
+    self.bottomLine.backgroundColor = [UIColor colorWithWhite:0.6 alpha:1].CGColor;
     
     [super layoutSubviews];
-    
+}
+
+- (BOOL)becomeFirstResponder {
+    BOOL isFirstResponder = [super becomeFirstResponder];
+    self.isActive = isFirstResponder;
+    [self layoutSubviews];
+    return isFirstResponder;
+}
+
+- (BOOL)resignFirstResponder {
+    BOOL resignedFirstResponder = [super resignFirstResponder];
+    self.isActive = !resignedFirstResponder;
+    [self layoutSubviews];
+    return resignedFirstResponder;
+}
+
+- (CALayer *)bottomLine {
+    if (!_bottomLine) {
+        _bottomLine = [CALayer layer];
+        _bottomLine.frame = CGRectMake(0.0f, self.frame.size.height - 1.0f, self.frame.size.width, 1.0f);
+        [self.layer addSublayer:_bottomLine];
+    }
+    return _bottomLine;
+}
+
+- (UIColor *)bottomLineColor {
+//    CGFloat alpha = self.isActive ? 1 : 0.5;
+    CGFloat alpha = 1;
+    return [UIColor colorWithWhite:0.6 alpha:alpha];
 }
 
 @end
