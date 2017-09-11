8fd287b1c7eac3fdf3dc0fa92515304e1c66c227
Rafael Damasceno <rdamasceno@poli.ufrj.br>
2017-09-11 01:13:00 -0300

    Atualiza a rota da api e static pages URLs

--- Caronae/CaronaeConstants.m
+++ Caronae/CaronaeConstants.m
@@ -3,9 +3,9 @@
 #pragma mark - Static pages URLs
 
 NSString *const CaronaeIntranetURLString = @"https://api.caronae.com.br/login";
-NSString *const CaronaeAboutPageURLString = @"https://api.caronae.ufrj.br/static_pages/sobre.html";
-NSString *const CaronaeTermsOfUsePageURLString = @"https://api.caronae.ufrj.br/static_pages/termos.html";
-NSString *const CaronaeFAQPageURLString = @"https://api.caronae.ufrj.br/static_pages/faq.html";
+NSString *const CaronaeAboutPageURLString = @"https://api.caronae.com.br/static_pages/sobre.html";
+NSString *const CaronaeTermsOfUsePageURLString = @"https://api.caronae.com.br/static_pages/termos.html";
+NSString *const CaronaeFAQPageURLString = @"https://api.caronae.com.br/static_pages/faq.html";
 
 
 #pragma mark - Notifications
